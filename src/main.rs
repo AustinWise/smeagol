4d464eddb2cc479b4c652f81f9fe2d1eab9c0959
Austin Wise <AustinWise@gmail.com>
2021-12-24 15:45:33 -0800

    Hook up web server.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use pulldown_cmark::{html, Options, Parser};
 
-fn main() {
+fn md() -> String {
     let markdown_input = "Hello world, this is a ~~complicated~~ *very simple* example.";
 
     // Set up options and parser. Strikethroughs are not part of the CommonMark standard
@@ -13,5 +13,38 @@ fn main() {
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
 
-    println!("{}", html_output);
+    html_output
+}
+
+use std::convert::Infallible;
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+
+async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from(md())))
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // pretty_env_logger::init();
+
+    // For every connection, we must make a `Service` to handle all
+    // incoming HTTP requests on said connection.
+    let make_svc = make_service_fn(|_conn| {
+        // This is the `Service` that will handle the connection.
+        // `service_fn` is a helper to convert a function that
+        // returns a Response into a `Service`.
+        async { Ok::<_, Infallible>(service_fn(hello)) }
+    });
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
